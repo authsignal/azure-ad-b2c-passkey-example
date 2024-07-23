e8ecda55d70fbc23a7de92266d6905ea7f9050e7
Aaron Zhong <aaronzhongnz@gmail.com>
2024-07-23 15:44:06 +1200

    Add Azure AD B2C example

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+
+const tenantId = process.env.AZURE_AD_B2C_TENANT_NAME!;
+
+const policy = "B2C_1A_SIGNUP_SIGNIN";
+
+const issuer = `https://${tenantId}.b2clogin.com/${tenantId}.onmicrosoft.com/v2.0`;
+
+export default NextAuth({
+  providers: [
+    {
+      id: "azure-ad-b2c",
+      name: "Azure Active Directory B2C",
+      type: "oauth",
+      wellKnown: `${issuer}/.well-known/openid-configuration?p=${policy}`,
+      idToken: true,
+      clientId: process.env.AZURE_AD_B2C_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET!,
+      authorization: { params: { scope: "offline_access openid" } },
+      profile: (profile) => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+        };
+      },
+    },
+  ],
+});
---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function HomePage() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <main>
+        <section>Loading...</section>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <section>
+        <h1>My Example App</h1>
+        {session?.user ? (
+          <>
+            <div>Signed in as: {session.user.name}</div>
+            <div>Email: {session.user.email}</div>
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={() =>
+                signIn("azure-ad-b2c", undefined, { prompt: "login" })
+              }
+            >
+              Sign in
+            </button>
+          </>
+        )}
+      </section>
+    </main>
+  );
+}
